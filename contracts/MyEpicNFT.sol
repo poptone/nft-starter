e4c3d743df510dc2ef5866cb1a596e0de8449044
poptones <flacko.jodye27@gmail.com>
2022-02-10 18:27:27 +0100

    pragma

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "hardhat/console.sol";
 
 import { Base64 } from "./libraries/Base64.sol";
